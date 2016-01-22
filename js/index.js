372b652c8b59f44ffb06dc0bb7fabcf4e97789ce
Brian Yeicol Restrepo Tangarife <byrestrepo@utp.edu.co>
2016-01-22 13:00:55 -0500

    Ajustes de opciones en el index

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Se encarga de autenticar al usuario
+ */
+(function($) {
+    var html;
+    if (localStorage.getItem('frontendPRSession')) {
+        html = '<a href="listar-preguntas.html"  class="btn-large waves-effect waves-light orange">Ver Preguntas</a>';
+    } else {
+        html = '<a href="login.html"  class="btn-large waves-effect waves-light light-blue darken-1">Login</a> ' +
+            '   <a href="resgitro.html"  class="btn-large waves-effect waves-light orange">Registrarse</a>';
+    }
+    $("#opciones-inicio").append(html);
+
+}(jQuery));
