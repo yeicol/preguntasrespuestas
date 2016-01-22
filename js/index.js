66032b7a26aa56bcf3854e4875479708a3097539
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-01-22 16:10:09 -0500

    corrección error url

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
         html = '<a href="listar-preguntas.html"  class="btn-large waves-effect waves-light orange">Ver Preguntas</a>';
     } else {
         html = '<a href="login.html"  class="btn-large waves-effect waves-light light-blue darken-1">Login</a> ' +
-            '   <a href="resgitro.html"  class="btn-large waves-effect waves-light orange">Registrarse</a>';
+            '   <a href="registro.html"  class="btn-large waves-effect waves-light orange">Registrarse</a>';
     }
     $("#opciones-inicio").append(html);
 
