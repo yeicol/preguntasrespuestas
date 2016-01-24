a9bf138304bda77d7a99a71c4a469ebb103c4fff
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-01-23 20:16:54 -0500

    HTML y JS para agregar pregunta

---
+++ js/agregar-pregunta.js
@@ -0,0 +1,33 @@
+/**
+ * Crea una pregunta
+ */
+(function ($) {
+  var url = 'http://preguntasrespuestas-yeicores72.rhcloud.com/api/preguntas';
+  $('#contenedor-formulario').on('submit', '#enviar-formulario',function () {
+    var contenido = JSON.stringify({
+      "contenido"     : $('#login').val()
+    });
+    $.ajax({
+      type: 'POST',
+      url: url,
+      dataType: "json",
+      contentType: 'application/json',
+      data: contenido,
+      xhrFields:   {
+         withCredentials: true
+     },
+      success: function(data, status, jqXHR)
+      {
+        window.location = 'agregar-pregunta.html';
+      },
+      error: function(data, status, jqXHR)
+      {
+        console.log(status);
+      },
+    });
+    
+    return false;
+  }); 
+
+
+}(jQuery));
