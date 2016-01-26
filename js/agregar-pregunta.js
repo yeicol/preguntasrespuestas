6378b15dca3cb6fdad6263e9644eb6acb092ee1c
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-01-26 13:39:12 -0500

    Formato

--- js/agregar-pregunta.js
+++ js/agregar-pregunta.js
@@ -1,31 +1,31 @@
 /**
- * Crea una pregunta
- */
+	* Crea una pregunta
+	*/
 (function ($) {
-	var url = 'http://preguntasrespuestas-yeicores72.rhcloud.com/api/preguntas';
-	$('#contenedor-formulario').on('submit', '#enviar-formulario', function () {
-		var contenido = JSON.stringify({
-			"titulo": $('#titulo').val(),
-			"contenido": $('#contenido').val()
-		});
-		$.ajax({
-			type: 'POST',
-			url: url,
-			dataType: "json",
-			contentType: 'application/json',
-			data: contenido,
-			xhrFields: {
-				withCredentials: true
-			},
-			success: function (data, status, jqXHR) {
-				window.location = 'agregar-pregunta.html';
-			},
-			error: function (data, status, jqXHR) {
-				console.log(status);
-			},
-		});
+		var url = 'http://preguntasrespuestas-yeicores72.rhcloud.com/api/preguntas';
+		$('#contenedor-formulario').on('submit', '#enviar-formulario', function () {
+				var contenido = JSON.stringify({
+						"titulo": $('#titulo').val(),
+						"contenido": $('#contenido').val()
+				});
+				$.ajax({
+						type: 'POST',
+						url: url,
+						dataType: "json",
+						contentType: 'application/json',
+						data: contenido,
+						xhrFields: {
+								withCredentials: true
+						},
+						success: function (data, status, jqXHR) {
+								window.location = 'agregar-pregunta.html';
+						},
+						error: function (data, status, jqXHR) {
+								console.log(status);
+						},
+				});
 
-		return false;
-	});
+				return false;
+		});
 
 }(jQuery));
