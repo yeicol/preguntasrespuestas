0dde1026a897a80422f61cccce8a1789e8c68980
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2016-01-27 13:12:55 -0500

    Visualización de preguntas del usuario loggeado

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,43 +1,48 @@
+/* global Materialize */
+
 (function ($) {
-	$('.template').each(function () {
-		var template = $(this).attr('data-template');
-		if (template.length > 0) {
-			if ($(this).attr('id') === 'navegacion') {
-				var titulo = $(this).attr('data-titulo');
-				$(this).load('templates/' + template, function () {
-					$('.nav-wrapper > #titulo-pagina').html(titulo);
-					$('.nav-wrapper > ul > li > a').filter(function () {
-						return $(this).text() === titulo;
-					}).parent().addClass('active');
-					menusVisibles();
+		$('.template').each(function () {
+				var template = $(this).attr('data-template');
+				if (template.length > 0) {
+						if ($(this).attr('id') === 'navegacion') {
+								var titulo = $(this).attr('data-titulo');
+								$(this).load('templates/' + template, function () {
+										$('.nav-wrapper > #titulo-pagina').html(titulo);
+										$('.nav-wrapper > ul > li > a').filter(function () {
+												return $(this).text() === titulo;
+										}).parent().addClass('active');
+										menusVisibles();
+								});
+						} else {
+								$(this).load('templates/' + template);
+						}
+				}
+		});
+
+		function menusVisibles() {
+				var frontendPRSession = parseInt(localStorage.getItem('frontendPRSession'), 10);
+				if (frontendPRSession) {
+						$('.nav-wrapper > ul > li > a[data-session="false"]').parent().hide();
+						$('a.guardar-id[data-session="true"]').attr('data-id', frontendPRSession);
+				} else {
+						$('.nav-wrapper > ul > li > a[data-session="true"]').parent().hide();
+				}
+				almacenarDatos();
+		}
+
+		function almacenarDatos() {
+				$('a.guardar-id').click(function (event) {
+						event.preventDefault();
+						var tipo = $(this).attr('data-tipo');
+						var id = $(this).attr('data-id');
+						localStorage.setItem('frontendPRTipo', tipo);
+						localStorage.setItem('frontendPRid', id);
+						window.location = $(this).attr('href');
 				});
-			} else {
-				$(this).load('templates/' + template);
-			}
 		}
-	});
-
-	function menusVisibles() {
-		var frontendPRSession = parseInt(localStorage.getItem('frontendPRSession'), 10);
-		if (frontendPRSession) {
-			$('.nav-wrapper > ul > li > a[data-session="false"]').parent().hide();
-			$('a.guardar-id').attr('data-id', frontendPRSession);
-		} else {
-			$('.nav-wrapper > ul > li > a[data-session="true"]').parent().hide();
+		var notificacion = localStorage.getItem('frontendPRnotificacion');
+		if (notificacion) {
+				Materialize.toast(notificacion, 3000, 'rounded');
+				localStorage.removeItem('frontendPRnotificacion');
 		}
-	}
-
-	$("#contenido-dinamico").on('click', 'a.guardar-id', function (event) {
-		event.preventDefault();
-		var tipo = $(this).attr('data-tipo');
-		var id = $(this).attr('data-id');
-		localStorage.setItem('frontendPRTipo', tipo);
-		localStorage.setItem('frontendPRid', id);
-		window.location = $(this).attr('href');
-	});
-	var notificacion = localStorage.getItem('frontendPRnotificacion');
-	if (notificacion) {
-		Materialize.toast(notificacion, 3000, 'rounded');
-		localStorage.removeItem('frontendPRnotificacion');
-	}
 }(jQuery));
