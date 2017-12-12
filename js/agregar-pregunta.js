740d29b381a2afa91e4e62f56ef5c3fc842cb0bd
Brian Yeicol Restrepo Tangarife <yeicores72@gmail.com>
2017-12-12 14:47:25 -0500

    Identación

--- js/agregar-pregunta.js
+++ js/agregar-pregunta.js
@@ -1,59 +1,59 @@
 /**
 	* Crea una pregunta
 	*/
-(function ($) {
+	(function ($) {
 		var url = 'http://preguntasrespuestas-yeicores72.rhcloud.com/api/preguntas';
 		var frontendPRSession = localStorage.getItem('frontendPRSession');
 		if (frontendPRSession) {
-				$('#contenedor-formulario').on('submit', '#enviar-formulario', function () {
-						var contenido = JSON.stringify({
-								"titulo": $('#titulo').val(),
-								"contenido": $('#contenido').val()
-						});
-						$.ajax({
-								type: 'POST',
-								url: url,
-								dataType: "json",
-								contentType: 'application/json',
-								data: contenido,
-								xhrFields: {
-										withCredentials: true
-								},
-								success: function (data) {
-										localStorage.setItem('frontendPRnotificacion', 'Su Pregunta ha sido guardada');
-										localStorage.setItem('frontendPRTipo', 'pregunta');
-										localStorage.setItem('frontendPRid', data.pregunta.id);
-										window.location = 'editar-pregunta.html';
-								},
-								error: function (data) {
-										switch (data.status) {
-												case 401:
-														localStorage.setItem('frontendPRRedirect', 'agregar-pregunta.html');
-														if (localStorage.getItem('frontendPRSession')) {
-																localStorage.removeItem('frontendPRSession');
-																localStorage.setItem('frontendPRnotificacion', 'Su sesión ha expirado');
-														} else {
-																localStorage.setItem('frontendPRnotificacion', 'Es necesario iniciar sesión');
-														}
-														window.location = 'login.html';
-														break;
-												case 404:
-														window.location = '404.html';
-														break;
-												case 500:
-														window.location = '500.html';
-												default:
-														Materialize.toast(JSON.parse(data.responseText).error, 3000, 'rounded');
-										}
-								},
-						});
-
-						return false;
+			$('#contenedor-formulario').on('submit', '#enviar-formulario', function () {
+				var contenido = JSON.stringify({
+					"titulo": $('#titulo').val(),
+					"contenido": $('#contenido').val()
 				});
+				$.ajax({
+					type: 'POST',
+					url: url,
+					dataType: "json",
+					contentType: 'application/json',
+					data: contenido,
+					xhrFields: {
+						withCredentials: true
+					},
+					success: function (data) {
+						localStorage.setItem('frontendPRnotificacion', 'Su Pregunta ha sido guardada');
+						localStorage.setItem('frontendPRTipo', 'pregunta');
+						localStorage.setItem('frontendPRid', data.pregunta.id);
+						window.location = 'editar-pregunta.html';
+					},
+					error: function (data) {
+						switch (data.status) {
+							case 401:
+							localStorage.setItem('frontendPRRedirect', 'agregar-pregunta.html');
+							if (localStorage.getItem('frontendPRSession')) {
+								localStorage.removeItem('frontendPRSession');
+								localStorage.setItem('frontendPRnotificacion', 'Su sesión ha expirado');
+							} else {
+								localStorage.setItem('frontendPRnotificacion', 'Es necesario iniciar sesión');
+							}
+							window.location = 'login.html';
+							break;
+							case 404:
+							window.location = '404.html';
+							break;
+							case 500:
+							window.location = '500.html';
+							default:
+							Materialize.toast(JSON.parse(data.responseText).error, 3000, 'rounded');
+						}
+					},
+				});
+
+				return false;
+			});
 		} else {
-				localStorage.setItem('frontendPRRedirect', 'agregar-pregunta.html');
-				localStorage.setItem('frontendPRnotificacion', 'Es necesario iniciar sesión');
-				window.location = 'login.html';
+			localStorage.setItem('frontendPRRedirect', 'agregar-pregunta.html');
+			localStorage.setItem('frontendPRnotificacion', 'Es necesario iniciar sesión');
+			window.location = 'login.html';
 		}
 
-}(jQuery));
+	}(jQuery));
